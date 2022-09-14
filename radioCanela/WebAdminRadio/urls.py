8a9416f1ad83184f23c15930e82d2c4ec716b61b
Matias Vasconez <matvguer@espol.edu.ec>
2022-09-14 18:55:13 -0500

    Se comenzo a trabajar en la vista emisora

--- radioCanela/WebAdminRadio/urls.py
+++ radioCanela/WebAdminRadio/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.administrador, name='administrador'),  # Muestra la pantalla principal /webadmin/
+    path('emisoras', views.emisoras, name='emisoras'),
+    path('emisoras/agregar', views.agregar_emisora, name="agregar_emisora"), # Muestra la pantalla para agregar emisora
 ]
