20090a593c35bc17e33c24aececd770ef7f8cc61
Matias Vasconez <matvguer@espol.edu.ec>
2022-09-16 19:15:02 -0500

    se trabajo en la vista emisoras

--- radioCanela/WebAdminRadio/urls.py
+++ radioCanela/WebAdminRadio/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.administrador, name='administrador'),  # Muestra la pantalla principal /webadmin/
-    path('emisoras', views.emisoras, name='emisoras'),
-    path('emisoras/agregar', views.agregar_emisora, name="agregar_emisora"), # Muestra la pantalla para agregar emisora
-    path('emisoras/<int:pk>/editar', views.editar_emisora, name="editar_emisora"), # Muestra la pantalla para agregar emisora
+    path('emisoras/', views.emisoras, name='emisoras'),
+    path('emisoras/agregar/', views.agregar_emisora, name="agregar_emisora"), # Muestra la pantalla para agregar emisora
+    path('emisoras/<int:pk>/editar/', views.editar_emisora, name="editar_emisora"), # Muestra la pantalla para agregar emisora
     path('equipos', views.equipos, name='equipos'), # URL para ver los equipos del sistema,
     path('equipos/agregar', views.agregar_equipo, name='agregar_equipo'), # Muestra la pantalla para agregar equipo
     # path('programas', views.segmentos, name='segmentos'), # Página principal donde se muestran los segmentos
