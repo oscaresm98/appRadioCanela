8497b50166414c814f687f4f1520aa161ea438fd
oscaresm98 <osedsanc@espol.edu.ec>
2022-10-25 17:51:38 -0500

    Correcion emisoras

--- radioCanela/WebAdminRadio/templatetags/webadmin_tags.py
+++ radioCanela/WebAdminRadio/templatetags/webadmin_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Programa, Horario
+from ..models import Programa, Horario, SegmentoEmisora, SegmentoLocutor, TelefonoEmisora
 
 register = template.Library()
 
@@ -7,3 +7,22 @@ register = template.Library()
 @register.simple_tag
 def get_horarios(programa):
     return Horario.objects.filter(id_programa=programa.id)
+
+# Devuelve el teléfono de una emisora
+@register.simple_tag
+def get_telf_emisora(emisora):
+    return TelefonoEmisora.objects.filter(id_emisora=emisora.id)
+
+# Devuelve la cantidad de segmentos de una emisora
+@register.simple_tag
+def get_cant_segmentos(emisora):
+    return Programa.objects.filter(pk__in=SegmentoEmisora.objects.filter(emisora=emisora).values('segmento'), estado=True).count()
+
+# Devuelve la cantidad de locutores de una emisora
+@register.simple_tag
+def get_cant_locutores(emisora):
+    count = 0
+    list_segmentos = Programa.objects.filter(pk__in=SegmentoEmisora.objects.filter(emisora=emisora).values('segmento'), estado=True)
+    for segmento in list_segmentos:
+        count += SegmentoLocutor.objects.filter(id_segmento=segmento.id).count()
+    return count
