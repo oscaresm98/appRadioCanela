1150e8b92f6e0acf8406d54b8217730e8e3dfb0f
santiago16171 <saantumb@espol.edu.ec>
2022-10-25 15:29:52 -0500

    eliminacion carpeta extra

--- Movil/src/app/principal/principal-routing.module.ts
+++ Movil/src/app/principal/principal-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-
 import { PrincipalPage } from './principal.page';
 
 const routes: Routes = [
