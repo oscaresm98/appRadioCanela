ad19c2fb374818db8a9ffa61fefb3af530700d9a
Matias Vasconez <matvguer@espol.edu.ec>
2022-12-05 14:17:30 -0500

    Mejoras podcast admin

--- radioCanela/WebAdminRadio/static/webAdminRadio/js/datatable_podcasts.js
+++ radioCanela/WebAdminRadio/static/webAdminRadio/js/datatable_podcasts.js
@@ -24,7 +24,7 @@ function getSegmentos(emisora) {
             { data: "descripcion"},
             { data: "fecha"},
             { data: "audio"},
-            { data: "activo"},
+            { data: "estado"},
             { data: "id"},
         ],
         columnDefs: [
@@ -32,12 +32,19 @@ function getSegmentos(emisora) {
             { width: 200, targets: 1},
             { width: 250, targets: 2},
             { width: 150, targets: 3, render: function(data) {
-                console.log("DATA: ",data)
-                if(data==null) return "null"
+                if(data==null) return "Vacio"
                 return `${data.slice(0,10)}`;
             }},
-            { width: 250, targets: 4},
-            { width: 200, targets: 5},
+            { width: 250, targets: 4, render: function(data) {
+                // console.log("Data audio",data)
+                if(data==null) return "Vacio"
+                return `<audio controls><source src="`+ data +`"></audio>`
+            }},
+            { width: 200, targets: 5,redner: function(data){
+                console.log("Data activo",data)
+                if(data==true) return "Si"
+                else return "No"
+            }},
             { width: 100, className: "text-center", targets: 6, render: function(data){
                 return `<a href="/podcasts/` + data + `/editar" class="btn btn-success btn-sm" role="button"><i class="fas fa-pen mx-auto"></i></a>
                 <a href="#" onclick="showWarning(` + data + `)" class="btn btn-danger btn-sm" role="button"><i class="fas fa-times mx-auto"></i></a>`
